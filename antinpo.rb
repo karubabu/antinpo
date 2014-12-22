38498fffcd094fa24f2d092a2c4b54327349f797
karubabu <karubabu8810@gmail.com>
2014-12-22 19:14:31 +0900

    ちょっと変数名いじった

--- antinpo.rb
+++ antinpo.rb
@@ -4,9 +4,9 @@ Plugin.create(:antinpo) do
 		ms.each do |m|
 			if m.message.to_me?()
 				if m.message.to_s =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
-					$exptmp=m.message.to_s
-					$exptmp=~ s/ちんぽ/なんで/
-					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + $exptmp + "?"*rand(40)}", :replyto => m)
+					exptmp=m.message.to_s
+					exptmp=~ s/ちんぽ/なんで/
+					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "?"*rand(40)}", :replyto => m)
 					m.message.favorite(true)
 				end
 			end
