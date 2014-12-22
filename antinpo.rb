9366624d083eb1b38bcc9dece5b4e6599a8703ce
karubabu <karubabu8810@gmail.com>
2014-12-22 19:11:41 +0900

    とりあえずできた

---
+++ antinpo.rb
@@ -0,0 +1,15 @@
+Plugin.create(:antinpo) do
+	DEFINED_TIME = Time.new.freeze
+	on_appear do |ms|
+		ms.each do |m|
+			if m.message.to_me?()
+				if m.message.to_s =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					$exptmp=m.message.to_s
+					$exptmp=~ s/ちんぽ/なんで/
+					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + $exptmp + "?"*rand(40)}", :replyto => m)
+					m.message.favorite(true)
+				end
+			end
+		end
+	end
+end
