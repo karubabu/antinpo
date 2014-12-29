107ce152f7090e0eb86f22c6cf0051ff9404f261
karubabu <karubabu8810@gmail.com>
2014-12-30 03:51:54 +0900

    no message

--- antinpo.rb
+++ antinpo.rb
@@ -4,14 +4,16 @@ Plugin.create(:antinpo) do
 	on_appear do |ms|
 		ms.each do |m|
 			if m.message.to_me?()
-				if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet? then
-				elsif m.message.to_s =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
-					exptmp = m.message.to_show()
-					exptmp = exptmp.gsub(/@.*\s/,'')
-					exptmp = exptmp.gsub(/ちんぽ/,'なんで')
-					exptmp = exptmp.gsub(/!|！/,'？')
-					Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(30)}", :replyto => m)
-					m.message.favorite(true)
+				if !m.message.from_me?()
+					if m.message.to_s =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif m.message.to_s =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+						exptmp = m.message.to_show()
+						exptmp = exptmp.gsub(/@.*\s/,'')
+						exptmp = exptmp.gsub(/ちんぽ/,'なんで')
+						exptmp = exptmp.gsub(/!|！/,'？')
+						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(30)}", :replyto => m)
+						m.message.favorite(true)
+					end
 				end
 			end
 		end
