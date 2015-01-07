74d02db66a048ee892ec0b19f57c92c74df3559e
karubabu <karubabu8810@gmail.com>
2015-01-07 14:13:54 +0900

    ちん◯系に対応したよ

--- antinpo.rb
+++ antinpo.rb
@@ -7,11 +7,16 @@ Plugin.create(:antinpo) do
 				if !m.message.from_me?()
 					exptmp = m.message.to_show()
 					exptmp = exptmp.gsub(/͏/,'')
+					exptmp = exptmp.gsub(/@.*\s/,'')
+					exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')
 					if exptmp =~ /ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					elsif exptmp =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
-						exptmp = exptmp.gsub(/@.*\s/,'')
-						exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')
 						exptmp = exptmp.gsub(/ちんぽ/,'なんで')
+						exptmp = exptmp.gsub(/!|！/,'？')
+						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
+						m.message.favorite(true)
+					elsif exptmp =~ /ちん(.+)?|(ちん){,2}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+						exptmp = exptmp.gsub(/ちん/,'なん')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
