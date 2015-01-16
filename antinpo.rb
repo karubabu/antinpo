bd0c17e0a4f75fdb40702c526dd90c3e17f823bd
karubabu <karubabu8810@gmail.com>
2015-01-17 00:24:54 +0900

    "ちんでも"に対応したつもり後スペースにも対応したよ

--- antinpo.rb
+++ antinpo.rb
@@ -9,15 +9,16 @@ Plugin.create(:antinpo) do
 					exptmp = exptmp.gsub(/͏/,'')
 					exptmp = exptmp.gsub(/@.*\s/,'')
 					exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')
+					exptmp = exptmp.gsub(/ |　/,'')
 					if exptmp =~ /ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					elsif exptmp =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/ちんぽ/,'なんで')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
-					elsif exptmp =~ /ちん(.+)?|(ちん){2,}|(ㄘん){2,}|ㄘん(.+)?|[tinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
-						exptmp = exptmp.gsub(/tinpo/,'nande')
-						exptmp = exptmp.gsub(/ｔｉｎｐｏ/,'ｎａｎｎｄｅ')
+					elsif exptmp =~ /ちん([.+^で])?|(ちん){2,}|(ㄘん){2,}|ㄘん(.+)?|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+						exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
+						exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｎｄｅ')
 						exptmp = exptmp.gsub(/ちん/,'なん')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
