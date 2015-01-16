a2dd6836d9b712eb543624f6e49661236e43896f
karubabu <karubabu8810@gmail.com>
2015-01-17 03:31:14 +0900

    ちんでも，ちんぽもに対応できたとおもう

--- antinpo.rb
+++ antinpo.rb
@@ -11,11 +11,11 @@ Plugin.create(:antinpo) do
 					exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')#"(@karubabu)"とかしてくるのを消す
 					exptmp = exptmp.gsub(/ |　/,'')#空白文字を消す
 					exptmp = exptmp.gsub(/!|！/,'？')
-					exptmp = exptmp.gsub(/ちんぽ[^も]/,'なんで')
+					exptmp = exptmp.gsub(/ちんぽも?/,'なんで')
 					exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
 					exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｄｅ')
-					exptmp = exptmp.gsub(/ちん[^で]/,'なん')
-					exptmp = exptmp.gsub(/ㄘん[^で]/,'なん')
+					exptmp = exptmp.gsub(/ちんで?/,'なん')
+					exptmp = exptmp.gsub(/ㄘんで?/,'なん')
 					if exptmp =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){5,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					elsif exptmp =~ /なんで|なん([.+^で])?|(なん){2,}|[nadeｎａｄｅ]{4,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
