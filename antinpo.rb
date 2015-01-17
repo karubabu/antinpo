ba8f995e3fb6a175958e9ed0d12c3664a1ea5bf4
karubabu <karubabu8810@gmail.com>
2015-01-17 14:38:22 +0900

    置換した後に確認するから関係ない文字列にも反応するし戻した

--- antinpo.rb
+++ antinpo.rb
@@ -6,18 +6,21 @@ Plugin.create(:antinpo) do
 			if m.message.to_me?()
 				if !m.message.from_me?()
 					exptmp = m.message.to_show()
-					exptmp = exptmp.gsub(/͏/,'')#なのはがやってきた謎の文字を消す
-					exptmp = exptmp.gsub(/@.*\s/,'')#リプライしてきた人のIDを消す
-					exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')#"(@karubabu)"とかしてくるのを消す
-					exptmp = exptmp.gsub(/ |　/,'')#空白文字を消す
-					exptmp = exptmp.gsub(/!|！/,'？')
-					exptmp = exptmp.gsub(/ちんぽ[^も]/,'なんで')
-					exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
-					exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｄｅ')
-					exptmp = exptmp.gsub(/ちん[^で]/,'なん')
-					exptmp = exptmp.gsub(/ㄘん[^で]/,'なん')
-					if exptmp =~ /ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){5,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
-					elsif exptmp =~ /なんで|なん([.+^で])?|(なん){2,}|[nadeｎａｄｅ]{4,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					exptmp = exptmp.gsub(/͏/,'')
+					exptmp = exptmp.gsub(/@.*\s/,'')
+					exptmp = exptmp.gsub(/[\(（]@.*[\)）]/,'')
+					exptmp = exptmp.gsub(/ |　/,'')
+					if exptmp =~ /ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif exptmp =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+						exptmp = exptmp.gsub(/ちんぽ/,'なんで')
+						exptmp = exptmp.gsub(/!|！/,'？')
+						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
+						m.message.favorite(true)
+					elsif exptmp =~ /ちん([^で]*)?|(ちん){2,}|(ㄘん){2,}|ㄘん([^で]*)?|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+						exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
+						exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｎｄｅ')
+						exptmp = exptmp.gsub(/ちん/,'なん')
+						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
 					end
