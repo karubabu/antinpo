1a4fe2277769c5a1f4e94516775b5822d14be4a6
karubabu <karubabu8810@gmail.com>
2015-01-23 04:27:54 +0900

    ちん　単体で返さないようになった

--- antinpo.rb
+++ antinpo.rb
@@ -17,7 +17,7 @@ Plugin.create(:antinpo) do
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
-					elsif exptmp =~ /(チン){2,}|チン([^でデ]*?)|ちん([^でデ]*?)|(ちん){2,}|(ㄘん){2,}|ㄘん([^でデ]*?)|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif exptmp =~ /(チン){2,}|チン([^でデ]+?)|ちん([^でデ]+?)|(ちん){2,}|(ㄘん){2,}|ㄘん([^でデ]+?)|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
 						exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｎｄｅ')
 						exptmp = exptmp.gsub(/ちん/,'なん')
