3778c203018c460dca6a95116e946e90071314e8
karubabu <karubabu8810@gmail.com>
2015-01-23 04:11:11 +0900

    カタカナを返せるようになった

--- antinpo.rb
+++ antinpo.rb
@@ -10,16 +10,18 @@ Plugin.create(:antinpo) do
 					exptmp = exptmp.gsub(/@.*?\p{blank}/,'')
 					exptmp = exptmp.gsub(/\p{blank}+/,'')
 					exptmp = exptmp.gsub(/͏/,'')
-					if exptmp =~ /ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
-					elsif exptmp =~ /ちんぽ/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					if exptmp =~ /チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif exptmp =~ /ち(\p{blank}+)ん(\p{blank}+)ぽ|チ(\p{blank}+)ン(\p{blank}+)ポ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/ち(\p{blank}+)ん(\p{blank}+)ぽ/,'な$1ん$2で')
+						exptmp = exptmp.gsub(/チ(\p{blank}+)ン(\p{blank}+)ポ/,'ナ$1ン$2デ')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
-					elsif exptmp =~ /ちん([^で]*)?|(ちん){2,}|(ㄘん){2,}|ㄘん([^で]*)?|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif exptmp =~ /(チン){2,}|チン([^でデ]*)+?|ちん([^でデ]*)+?|(ちん){2,}|(ㄘん){2,}|ㄘん([^でデ]*)?|[cｃtinpoｔｉｎｐｏ]{5,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/tinpo|cinpo/,'nande')
 						exptmp = exptmp.gsub(/ｔｉｎｐｏ|ｃｉｎｐｏ/,'ｎａｎｎｄｅ')
 						exptmp = exptmp.gsub(/ちん/,'なん')
+						exptmp = exptmp.gsub(/チン/,'ナン')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
