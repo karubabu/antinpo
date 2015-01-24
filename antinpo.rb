a8ce74f2f1024c81eb47eb6d50d2d5fd74eb8387
karubabu <karubabu8810@gmail.com>
2015-01-24 15:23:13 +0900

    no message

--- antinpo.rb
+++ antinpo.rb
@@ -10,14 +10,12 @@ Plugin.create(:antinpo) do
 					exptmp = exptmp.gsub(/@.*?\p{blank}/,'')
 					exptmp = exptmp.gsub(/\p{blank}+?/,'')
 					exptmp = exptmp.gsub(/͏/,'')
-					if exptmp =~ /ﾁﾝﾎﾟﾓ|チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					if exptmp =~ /チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
 					elsif exptmp =~ /ち(\p{blank}*?)ん(\p{blank}*?)ぽ|チ(\p{blank}*?)ン(\p{blank}*?)ポ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/ち(\p{blank}*?)ん(\p{blank}*?)ぽ/,'な' + $1 +'ん' + $2 + 'で')
 						exptmp = exptmp.gsub(/チ(\p{blank}*?)ン(\p{blank}*?)ポ/,'ナ' + $1 +'ン' + $2 + 'デ')
-						exptmp = exptmp.gsub(/ﾁ(\p{blank}*?)ﾝ(\p{blank}*?)ﾎﾟ/,'ﾅ' + $1 +'ﾝ' + $2 + 'ﾃﾞ')
 						exptmp = exptmp.gsub(/ぽ/,'で')
 						exptmp = exptmp.gsub(/ポ/,'デ')
-						exptmp = exptmp.gsub(/ﾎﾟ/,'ﾃﾞ')
 						exptmp = exptmp.gsub(/!|！/,'？')
 						Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + exptmp + "？"*rand(20)}", :replyto => m)
 						m.message.favorite(true)
