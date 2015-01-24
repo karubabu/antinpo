417095dbd0912ae8744931660e34acaadc3fef6b
karubabu <karubabu8810@gmail.com>
2015-01-24 14:57:17 +0900

    no message

--- antinpo.rb
+++ antinpo.rb
@@ -11,7 +11,7 @@ Plugin.create(:antinpo) do
 					exptmp = exptmp.gsub(/\p{blank}+/,'')
 					exptmp = exptmp.gsub(/͏/,'')
 					if exptmp =~ /ﾁﾝﾎﾟﾓ|チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａ]){3,}?|[!！]{10,}/ and m[:created] > DEFINED_TIME and !m.retweet? then
-					elsif exptmp =~ /(ち|チ|ﾁ)\p{blank}*?(ん|ン|ﾝ)\p{blank}*?(ぽ|ポ|ﾎﾟ)/ and m[:created] > DEFINED_TIME and !m.retweet? then
+					elsif exptmp =~ /ち(\p{blank}*?)ん(\p{blank}*?)ぽ|チ(\p{blank}*?)ン(\p{blank}*?)ポ|ﾁ(\p{blank}*?)ﾝ(\p{blank}*?)ﾎ/ and m[:created] > DEFINED_TIME and !m.retweet? then
 						exptmp = exptmp.gsub(/ち(\p{blank}*?)ん(\p{blank}*?)ぽ/,'な' + $1 +'ん' + $2 + 'で')
 						exptmp = exptmp.gsub(/チ(\p{blank}*?)ン(\p{blank}*?)ポ/,'ナ' + $1 +'ン' + $2 + 'デ')
 						exptmp = exptmp.gsub(/ﾁ(\p{blank}*?)ﾝ(\p{blank}*?)ﾎﾟ/,'ﾅ' + $1 +'ﾝ' + $2 + 'ﾃﾞ')
