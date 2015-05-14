cf02e4e8df6d6e49d1c28e6639ce037c7543084c
karubabu <karubabu8810@gmail.com>
2015-05-14 19:45:07 +0900

    なんかした

--- antinpo.rb
+++ antinpo.rb
@@ -56,7 +56,7 @@ checktmp = nil
 					checktmp=exptmp
 					checktmp = checktmp.gsub(/\p{blank}+?/,'')
 					# ﾌﾞﾁﾐﾘ系ではなく かつ ちんぽ系である
-					if checktmp !~ /チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａi]){5,}?|[!！]{10,}/ and
+					if checktmp !~ /チンポモ|ちんぽも|ﾌﾞﾘ|ﾘｭﾘｭﾘｭ|ﾌﾞﾂ|ﾁﾁ|ﾐﾘ|([うおあｕｏａio]){5,}?|[!！]{10,}/ and
 						checktmp =~ /[ㄘちんぽチンポﾁﾝﾎﾟ]{3,}|[TINPOｔｃｉｎｐｏtcinpo]{4,}|(チン|ちん|ﾁﾝ)([^でデﾃﾞ]+)/ and m[:created] > DEFINED_TIME and !m.retweet? then
 
 						exptmp=tinpoConverter(exptmp)
